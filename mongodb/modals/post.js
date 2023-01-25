d479eca56e7af42601a420ad329667d846ab8329
Julio Rojas <juliorojas81871@gmail.com>
2023-01-25 17:17:56 -0500

    finish

--- mongodb/modals/post.js
+++ mongodb/modals/post.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const Post = new mongoose.Schema({
-    name: {type:String, require: true},
-    prompt: {type:String, require: true},
-    photo: {type:String, require: true},
-})
+  name: { type: String, required: true },
+  prompt: { type: String, required: true },
+  photo: { type: String, required: true },
+});
 
-const PostSchema = mongoose.model("post", Post);
+const PostSchema = mongoose.model('Post', Post);
 
 export default PostSchema;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,10 +2,48 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 
-import Post from "../mongodb/modals/post";
+import Post from "../mongodb/modals/post.js";
 
 dotenv.config();
 
 const router = express.Router();
 
-export default router
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Get All Post
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Fetching posts failed, please try again",
+      });
+  }
+});
+
+//  Create A Post
+router.route("/").post(async (req, res) => {
+  const { name, prompt, photo } = req.body;
+  const photoUrl = await cloudinary.uploader.upload(photo);
+
+  try {
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+    res.status(200).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+export default router;
